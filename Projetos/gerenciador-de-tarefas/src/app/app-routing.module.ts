b299a3cbb30822729533a22859e62dfa0eefb15c
MATEUS-PC\Mateus <mateus.artur1597@gmail.com>
2018-04-02 10:11:14 -0300

    Angular

---
+++ Projetos/gerenciador-de-tarefas/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TarefasRoutes } from './tarefas'
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/tarefas/listar',
+        pathMatch: 'full'
+    },
+    ...TarefasRoutes
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule{
+    
+}
---
+++ Projetos/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { TarefaService, TarefaConcluidaDirective } from './shared';
+import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers:[
+    TarefaService
+  ],
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
+  ]
+})
+export class TarefasModule { }
